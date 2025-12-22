728d6ae19fdade9b16319e7e574f1889ce6dc45f
Kai7788 <Kaimaier2002@yahoo.com>
2025-12-22 13:19:00 +0100

    Zwischenstand

---
+++ setup/updateImages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker pull redis:alpine
+docker pull mariadb:10.6
+docker pull lscr.io/linuxserver/nextcloud:latest
+docker pull pihole/pihole:latest
+docker pull osminogin/tor-simple:latest
+
