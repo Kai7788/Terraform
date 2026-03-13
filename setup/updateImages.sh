d840536ff624404e21c155839fd054668463884f
Kai7788 <79104388+Kai7788@users.noreply.github.com>
2026-03-13 21:04:54 +0100

    Add Woodpecker CI server and agent images to script

--- setup/updateImages.sh
+++ setup/updateImages.sh
@@ -4,4 +4,5 @@ docker pull mariadb:10.6
 docker pull lscr.io/linuxserver/nextcloud:latest
 docker pull pihole/pihole:latest
 docker pull osminogin/tor-simple:latest
-
+docker pull woodpeckerci/woodpecker-server:v3
+docker pull woodpeckerci/woodpecker-agent:v3
